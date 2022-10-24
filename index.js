ba6c8a76999b9b095329ce27cc9a4e91c79d8784
Mahmud <rafsanchowdhuryrudro@gmail.com>
2022-10-25 02:35:56 +0600

    data create

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 5000;
+const courses = require('./data/courses.json')
 
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('hello world')
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
 })
 
 app.listen(port, () => {
