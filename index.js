2682ee64c54f0011192fe238a57d6f9cb10931ec
Mahmud <rafsanchowdhuryrudro@gmail.com>
2022-10-25 04:22:27 +0600

    dynamic id

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ app.get('/courses', (req, res) => {
     res.send(courses)
 })
 
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const course = courses.find(details => details.id === id);
+    res.send(course)
+})
+
 app.listen(port, () => {
     console.log('server is running')
 })
