e9d5537b4cc6870931cb5644e6a50921dcb7b32f
Mahmud <rafsanchowdhuryrudro@gmail.com>
2022-10-26 00:47:14 +0600

    readme file edit

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const port = 5000;
-const courses = require('./data/courses.json')
+const courses = require("./data/courses.json");
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('hello world')
-})
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
-app.get('/courses', (req, res) => {
-    res.send(courses)
-})
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
 
-app.get('/courses/:id', (req, res) => {
-    const id = req.params.id;
-    const course = courses.find(details => details.id === id);
-    res.send(course)
-})
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const course = courses.find((details) => details.id === id);
+  res.send(course);
+});
 
 app.listen(port, () => {
-    console.log('server is running')
-})
-
+  console.log("server is running");
+});
